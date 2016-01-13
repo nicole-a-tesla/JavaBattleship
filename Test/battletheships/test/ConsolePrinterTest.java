f3e225be7ba2119000a228148806ad45cf74b929
nicole-a-tesla <thisisnotnicole@gmail.com>
2016-01-13 11:28:44 -0500

    ConsolePrinter prints to the console

--- Test/battletheships/test/ConsolePrinterTest.java
+++ Test/battletheships/test/ConsolePrinterTest.java
@@ -1,7 +1,35 @@
 package battletheships.test;
 
+import battletheships.ConsolePrinter;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by bears8yourface on 1/12/16.
  */
 public class ConsolePrinterTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setup() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    public void printsToTheConsole() {
+        ConsolePrinter printer = new ConsolePrinter();
+        printer.print("I printed!");
+        assertEquals("I printed!", outContent.toString());
+    }
+
+    @After
+    public void cleanupStreams() {
+        System.setOut((null));
+    }
 }
--- src/battletheships/ConsolePrinter.java
+++ src/battletheships/ConsolePrinter.java
@@ -4,5 +4,7 @@ package battletheships;
  * Created by bears8yourface on 1/12/16.
  */
 public class ConsolePrinter extends Printer {
-
+    public void print(String string) {
+      System.out.print(string);
+    }
 }
